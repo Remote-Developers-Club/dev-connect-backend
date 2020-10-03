9d072ff46085aa92f40baa39d8074123177846e1
acquahsamuel <acquahsamuel1996@gmail.com>
2020-10-03 15:06:56 +0000

    routes handlers update

---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post();
+router.get();
+router.put();
+router.patch();
+
+
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post();
+router.get();
+router.put();
+router.patch();
+
+
+module.exports = router;
